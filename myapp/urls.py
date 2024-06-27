782c34c899a96aa970b8118eab90a59306eb2a30
clement <clemcy9@gmail.com>
2024-06-27 23:07:15 +0100

    basic rest template completed

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet
+from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from .models import Item
+
 
 router = DefaultRouter()
 router.register(r'items',ItemViewSet)
 
+class MyAdminSite(AdminSite):
+    site_header = "My Custom Admin"
+    site_title = "Admin Portal"
+    index_title = "Welcome to My Admin"
+
+my_admin_site = MyAdminSite(name='myadmin')
+
+my_admin_site.register(Item)
+
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('', include(router.urls)),
+    path('myadmin/', my_admin_site.urls )
 ]
